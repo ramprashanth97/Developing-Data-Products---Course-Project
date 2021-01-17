10973194c67acb9a29a740072fff1e7d94564aac
ramprashanth97 <64185912+ramprashanth97@users.noreply.github.com>
2021-01-17 19:31:00 +0530

    final presentation

--- server.R
+++ server.R
@@ -1,42 +1,43 @@
+#load required packages
 library(shiny)
-library(datasets)
+library(dplyr)
+suppressPackageStartupMessages(library(googleVis))
 
-mpgData <- mtcars
-mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
 
+#build dataframe for creating plot
+states <- data.frame(state.name, state.area, state.region,
+                     hover=paste0(state.name, ": ", 
+                                  state.area, " square miles"))
+states$hover <- as.character(states$hover)
+
+#Define server logic required to draw the plot
 shinyServer(function(input, output) {
   
-  formulaText <- reactive({
-    paste("mpg ~", input$variable)
-  })
-  
-  formulaTextPoint <- reactive({
-    paste("mpg ~", "as.integer(", input$variable, ")")
-  })
-  
-  fit <- reactive({
-    lm(as.formula(formulaTextPoint()), data=mpgData)
-  })
-  
-  output$caption <- renderText({
-    formulaText()
-  })
-  
-  output$mpgBoxPlot <- renderPlot({
-    boxplot(as.formula(formulaText()), 
-            data = mpgData,
-            outline = input$outliers)
-  })
-  
-  output$fit <- renderPrint({
-    summary(fit())
-  })
-  
-  output$mpgPlot <- renderPlot({
-    with(mpgData, {
-      plot(as.formula(formulaTextPoint()))
-      abline(fit(), col=2)
-    })
+  output$plot1 <- renderGvis({
+    
+    #Rename slider values
+    minarea <- input$area[1]
+    maxarea <- input$area[2]
+    
+    #create vector containing values from checkboxes
+    regions <- vector(length=4)
+    regions[1] <- ifelse(input$NE, "Northeast", "")
+    regions[2] <- ifelse(input$NC, "North Central", "")
+    regions[3] <- ifelse(input$S, "South", "")
+    regions[4] <- ifelse(input$W, "West", "")
+    
+    #filter data using slider and checkbox input
+    plotdata <- states %>% 
+      filter(state.area>minarea, state.area<maxarea) %>%
+      filter(state.region %in% regions)
+    
+    #plot map
+    gvisGeoChart(plotdata, 
+                 locationvar="state.name", hovervar="hover",
+                 options=list(region="US", displayMode="regions", 
+                              resolution="provinces",
+                              width=800, height=600))
+    
   })
   
 })
